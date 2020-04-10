de51a7c2c5f3f715551c026d884decfc2f9e7a87
Vladimir <vladimir.potekh@gmail.com>
2020-04-10 16:14:25 +0300

    chore(test): improve coverage

--- projects/resize-observer/src/services/tests/resize-observer.service.spec.ts
+++ projects/resize-observer/src/services/tests/resize-observer.service.spec.ts
@@ -1,24 +1,63 @@
-import {NgZone} from '@angular/core';
+import {ElementRef} from '@angular/core';
+import {TestBed} from '@angular/core/testing';
+import {catchError} from 'rxjs/operators';
+import {RESIZE_OBSERVER_SUPPORT} from '../../tokens/support';
 import {ResizeObserverService} from '../resize-observer.service';
 
-describe('ResizeObserverService', () => {
-    it('throws when not supported', () => {
-        let error = false;
-        const service = new ResizeObserverService(
-            {
-                nativeElement: document.createElement('DIV'),
-            },
-            new NgZone({}),
-            false,
-            'content-box',
-        );
+describe('Resize Observer token', () => {
+    let service: ResizeObserverService;
 
-        service.subscribe({
-            error: () => {
-                error = true;
-            },
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                ResizeObserverService,
+
+                {
+                    provide: ElementRef,
+                    useValue: {
+                        nativeElement: document.createElement('DIV'),
+                    },
+                },
+            ],
         });
 
-        expect(error).toBe(true);
+        service = TestBed.get(ResizeObserverService).pipe(
+            catchError((_err, caught) => caught),
+        );
+    });
+
+    it('defined', () => {
+        expect(service).toBeDefined();
+    });
+});
+
+describe('throws when not supported', () => {
+    it('Throws an error if ResizeObserver is not supported', done => {
+        TestBed.configureTestingModule({
+            providers: [
+                ResizeObserverService,
+
+                {
+                    provide: ElementRef,
+                    useValue: {
+                        nativeElement: document.createElement('DIV'),
+                    },
+                },
+                {
+                    provide: RESIZE_OBSERVER_SUPPORT,
+                    useValue: false,
+                },
+            ],
+        });
+
+        const service$: ResizeObserverService = TestBed.get(ResizeObserverService);
+
+        service$.subscribe({
+            error: err => {
+                expect(err).toBe('ResizeObserver is not supported in your browser');
+
+                done();
+            },
+        });
     });
 });
